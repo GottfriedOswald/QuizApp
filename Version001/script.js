4e4799e56ae639a96d0fd04dbf6b8ac2db2d7cff
GottfriedOswald <go.oswald@t-online.de>
2021-08-01 14:36:19 +0200

    include functions

--- Version001/script.js
+++ Version001/script.js
@@ -8,6 +8,7 @@
 //     "right_answer": 0
 // }
 
+// ---------die JSON-Variable enthält die Fragen und Antworten ------------
 let questions = [{
         "question": "Mit welchem Befehl ist es mir möglich einen Eintrag aus einem Array zu löschen?",
         "answer_1": "splice",
@@ -43,42 +44,93 @@ let questions = [{
     {
         "question": "Was ist ein JSON?",
         "answer_1": "ein Mitglied einer Boyband!",
-        "answer_2": "der Spitzname von Homer J. Simpson",
-        "answer_3": "Dabei handelt es sich um ein standardisiertes Datenformat für die Codierung von Daten",
+        "answer_2": "der Spitzname von Homer J. Simpson!",
+        "answer_3": "Dabei handelt es sich um ein standardisiertes Datenformat für die Codierung von Daten!",
         "answer_4": "ein alkoholfreies Getränk!",
         "right_answer": 3
+    },
+    {
+        "question": "Für was wird der Befehl 'slice' verwendet?",
+        "answer_1": "einen String einfrieren!",
+        "answer_2": "einen char, Teil eines String oder einen String aus einem Array extrahieren!",
+        "answer_3": "zum löschen eines Strings!",
+        "answer_4": "diesen Befehl gibt´s nicht!",
+        "right_answer": 2
     }
 ];
 
-let currentQuestion = 0;
+let currentQuestion = 0; //...................................................Variable für die Frage aus dem aktuellem JSON-Array-Eintrag 
+let points = 0; //............................................................Variable für die richtig beantworteten Fragen
+
+
 
 function init() {
-    document.getElementById('numberOfQuestions').innerHTML = questions.length;
+    document.getElementById('numberOfQuestions').innerHTML = questions.length; //.........Anzahl der Fragen wird dem html-Tag mit der id 'play-screen' übergeben
+    document.getElementById('questions-amount').innerHTML = questions.length; //.........Anzahl der Fragen wird dem html-Tag mit der id 'end-screen' übergeben
     showQuestion();
 }
 
-function showQuestion() {
+function showQuestion() { //.............................................................Fragen und mögliche Antworten werden den jeweiligen html-Tags übergeben
     let question = questions[currentQuestion];
     document.getElementById('questiontext').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
-    // showAnswers();
+
+    document.getElementById('next-button').disabled = true; //..........................der button für die nächste Frage wird deaktiviert
+
+    updateProgressbar();
+
 }
 
-// function showAnswers() {
-//     let answer = questions[currentQuestion];
-//     for (let i = 1; i < 5; i++) {
-//         document.getElementById(`answer_${i}`).innerHTML = answer[`answer_${i}`];
-//     }
-// }
 
-function checkAnswer(x) {
-    let answercheck = questions[currentQuestion];
-    if (x == answercheck['right_answer']) {
-        document.getElementById(`answer_${x}`).style.backgroundColor = "rgba(140,190,140,0.5";
+
+function checkAnswer(selection) {
+    let question = questions[currentQuestion]; //......................................Übergabe des aktuellen Arra-Eintrags an die neue Variable
+    let selectedQuestionNumber = selection.slice(-1); //...............................aus dem mitgeliefertem String (der bei der Übergabe in der Variablen 'selection')
+    //.................................................................................gespeichert wurde, wir mit 'splice' ein Teil des Strings (in diesem Fall das letzte Char-Zeichen)
+    //.................................................................................aus dem String extrahiert und der neuen Variable übergeben
+
+    let correctAnswer = `answer_${question['right_answer']}`; //.......................die korrekte Antort zur zugehörigen Frage wird in der neuen Variable gespeichert
+
+    if (selectedQuestionNumber == question['right_answer']) { //.......................die selektierte Antwort wird mit der korrekten Antwort verglichen
+        document.getElementById(selection).parentNode.classList.add('bg-success'); //..wenn Bedingung erfüllt (Antwort korrekt) dann wird mit 'parentNode' dem übergeordnetem Element (Elternelement) eine Klasse hinzugefügt.Antwort wird (grün) markiert.
+        points++; //...................................................................Anzahl der korreken Antworten wird erhöht.
     } else {
-        document.getElementById(`answer_${x}`).style.backgroundColor = "rgba(230,110,110,0.5";
+        document.getElementById(selection).parentNode.classList.add('bg-danger'); //........auch hier wird dem Elternelement eine Klasse hinzugefügt.Antwort wird rot markiert da falsche Antwort
+        document.getElementById(correctAnswer).parentNode.classList.add('bg-success'); //...und hier auch, die richtige Antwort wird grün markiert
     }
+    document.getElementById('next-button').disabled = false; //..........................der button für die nächste Frage wird aktiviert
+}
+
+function nextQuestion() {
+    if (currentQuestion < questions.length - 1) { //.....................................Abfrage aktueller Arra-Eintrag kleiner ist als Anzahl der Einträge im Array
+        currentQuestion++; //............................................................Array-Eintrag wird um 1 hochgezählt, quasi wird die nächste Frage gestellt
+        resetSelections(); //............................................................Funktionsaufruf zum entfernen der Markierung der Antwort
+        showQuestion(); //...............................................................Fragen und Antworten anzeigen
+        document.getElementById('questionNumber').innerHTML = currentQuestion + 1; //....Anzeige der wievielten Frage im 'play-Screen' (question-footer)
+    } else {
+        document.getElementById('play-screen').classList.add('d-none'); //...............wenn alle Fragen durch sind wird der play-screen entfernt
+        document.getElementById('end-screen').classList.remove('d-none'); //.............und der end-screen sichtbar gemacht
+        document.getElementById('points').innerHTML = points; //.........................die Anzahl der richtig beantworteten Fragen wir übergeben
+    }
+}
+
+function resetSelections() { //..........................................................in dieser Funktion werden die Antwortmarkierungen durch Entfernung der Klassen aus der Classlist entfernt
+    for (let i = 1; i < 5; i++) {
+        document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-danger');
+        document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-success');
+    }
+}
+
+function updateProgressbar() {
+    let percent = calcPercent();
+    document.getElementById('progress-bar').style = `width:${percent}%`;
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+}
+
+function calcPercent() {
+    let result = Math.round(((currentQuestion + 1) / questions.length) * 100);
+    return result;
 }
